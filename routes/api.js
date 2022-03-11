132b39fe4e1f4c50b9645da679e725b9b15f6aaf
Bas van Driel <bas.vandriel.nl@gmail.com>
2022-03-11 12:18:10 +0100

    add required fields for /api/threads/{board}

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,27 @@
 'use strict';
 
+const multer = require('multer')
+
+/**
+ * Used for using form-data in requests
+ */
+const upload = multer()
+
 module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
+
+  app.route('/api/threads/:board').post(upload.none(), (request, response) => {
+    // The required text and password
+    const { text, delete_password } = request.body
+
+    // The board we're adding
+    const board = request.params.board
+    const replies = []
+
+    // Idk it's required
+    const reported = false
+
+    return response.json({ data: { text, delete_password, board, replies, reported }, ok: true })
+  });
     
   app.route('/api/replies/:board');
 
